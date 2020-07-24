d8cb311f02664cb6c8995f9331f378d329dfea9d
BoguckiMich <bogucki.mich@gmail.com>
2020-07-24 10:25:23 +0200

    obliczenai azymutu oski

--- dxfRead.py
+++ dxfRead.py
@@ -1,4 +1,5 @@
 import ezdxf
+import math
 
 doc = ezdxf.readfile("./resources/ptycz.dxf");
 
@@ -13,8 +14,8 @@ def print_entity(e):
     # print("start point: %s\n" % e.dxf.x)
     # print("end point: %s\n" % e.dxf.y)
     for line in e:
-        print("x of line: %s\n" % line[0])
-        print("y of line: %s\n" % line[1])
+        # print("x of line: %s\n" % line[0])
+        # print("y of line: %s\n" % line[1])
         polyline_points.append([(line[0], line[1])])
 
 #oska musi byc na warstwie "os" i byc poliline
@@ -23,7 +24,13 @@ for e in msp:
         print_entity(e)
 
 #wydruka arraya ze wszystkimi punktami lini
-print(polyline_points)
-# for e in msp.query('LWPOLYLINE'):
-#     print_entity(e)
+# print(polyline_points)
 
+#obliczenei azymutu oski
+x1 = polyline_points[0][0][0]
+y1 = polyline_points[0][0][1]
+x2 = polyline_points[-1][0][0]
+y2 = polyline_points[-1][0][1]
+
+azimuth = math.atan((y2-y1)/(x2-x1))
+print(azimuth*180/math.pi)
